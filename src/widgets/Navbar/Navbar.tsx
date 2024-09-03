8b41345b6f2e54ea8cd3c518a46f654da994a924
softwarechecker <dikkten32@yandex.ru>
2024-09-03 20:46:10 +0400

    add menu burger

--- src/widgets/Navbar/Navbar.tsx
+++ src/widgets/Navbar/Navbar.tsx
@@ -2,11 +2,14 @@ import { Link } from 'react-router-dom';
 import { links } from './config/config';
 import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.scss';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isToggle, setIsToggle] = useState<boolean>(false);
+
   return (
     <header>
-      <nav className={styles.wrapper}>
+      <div className={styles.wrapper}>
         <div className={styles.logo}>
           <Link to="/">Krasnov & Kartashov</Link>
         </div>
@@ -25,7 +28,27 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-      </nav>
+        <div className={styles.burgerButton} onClick={() => setIsToggle(!isToggle)}>
+          <img src="menu-2.svg" alt="" />
+        </div>
+      </div>
+      {isToggle && (
+        <div className={styles.burgerMenu}>
+          <ul>
+            {links.map((link) => (
+              <li key={link.link}>
+                <NavLink
+                  to={link.link}
+                  className={({ isActive, isPending }) =>
+                    isPending ? styles.pending : isActive ? styles.active : ''
+                  }>
+                  {link.text}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </header>
   );
 };
